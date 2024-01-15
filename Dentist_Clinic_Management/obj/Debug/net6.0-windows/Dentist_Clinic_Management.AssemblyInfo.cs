0353c2c6a828b6c2c5ca53ecf2777dbf6c23080d
SvvPhenex <phucdat112003@gmail.com>
2024-01-15 20:10:48 +0700

    chinh sua ho so ca nhan cua nha si

--- Dentist_Clinic_Management/obj/Debug/net6.0-windows/Dentist_Clinic_Management.AssemblyInfo.cs
+++ Dentist_Clinic_Management/obj/Debug/net6.0-windows/Dentist_Clinic_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dentist_Clinic_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5b949d98d290114f8acf323218640e4ee64dfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e670be81ced2eecaee2b520a5b07026b61f88160")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dentist_Clinic_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dentist_Clinic_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
