60822bd21bfd86c5549b6439f458e73eb4f6faa8
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-01-15 19:28:34 +0700

    Update Info_Staff.cs

--- Dentist_Clinic_Management/Staff/Info_Staff.cs
+++ Dentist_Clinic_Management/Staff/Info_Staff.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dentist_Clinic_Management.DAO_Dentist_Clinic_Management;
+using Dentist_Clinic_Management_UI.DAO_Dentist_Clinic_Management;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +17,28 @@ namespace Dentist_Clinic_Management.Staff
         public Info_Staff()
         {
             InitializeComponent();
+            lab_incorrect.Hide();
+            label5.Hide();
+            tb_Name.Text = AccountDAO.Instance.ID;
+            textBox1.Text = AccountDAO.Instance.GetOtherName(AccountDAO.Instance.ID);
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
+            if (textBox3.Text != AccountDAO.Instance.Pass)
+            {
+                lab_incorrect.Show();
+                return;
+            }
+            string query = string.Format("update NguoiDung set MatKhau = '{0}' where MaNguoiDung = '{1}'", textBox2.Text, AccountDAO.Instance.ID);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            if (result > 0)
+                label5.Show();
+        }
 
+        private void tb_Name_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
