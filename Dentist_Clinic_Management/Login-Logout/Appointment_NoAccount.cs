03396dc8131404ee8426d7743ec12898efc3c845
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-01-15 19:30:38 +0700

    Update Appointment_NoAccount.cs

--- Dentist_Clinic_Management/Login-Logout/Appointment_NoAccount.cs
+++ Dentist_Clinic_Management/Login-Logout/Appointment_NoAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dentist_Clinic_Management.DAO_Dentist_Clinic_Management;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,17 @@ namespace Dentist_Clinic_Management.Login_Logout
 {
     public partial class Appointment_NoAccount : Form
     {
-       
+
         public Appointment_NoAccount()
         {
             InitializeComponent();
         }
-
+        DateTime date_chosse;
+        void LoadListDentist(DateTime date)
+        {
+            tb_dentist.DataSource = AppointDAO.Instance.GetListDentist(date);
+            tb_dentist.DisplayMember = "Hoten";
+        }
         private void bt_Register_Click(object sender, EventArgs e)
         {
             Register register = new Register();
@@ -30,49 +36,55 @@ namespace Dentist_Clinic_Management.Login_Logout
         {
             this.Close();
         }
-
+        bool Trung(string phone)
+        {
+            return AppointDAO.Instance.PhoneFine(phone);
+        }
         private void bt_rg_Register_Click(object sender, EventArgs e)
         {
             bool Error_Register = false;
-
-            if (tb_name.Text.Length == 0)
+            string name = tb_name.Text;
+            DateTime birth = tb_birth.Value;
+            string addr = tb_addr.Text;
+            string phone = tb_phone.Text;
+            string id2 = AppointDAO.Instance.GetIDDentist(tb_dentist.Text);
+            DateTime date = date_chosse;
+            if (Trung(phone))
             {
-                Error_Register = true;
-                MessageBox.Show("Vui lòng nhập họ và tên của bạn!", "Thông tin đặt hẹn không hợp lệ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Warming_Admin.Failed failed = new Warming_Admin.Failed();
+                failed.Show();
             }
-            if (Error_Register == false)
+            else
             {
-                if (tb_addr.Text.Length == 0)
+                if (AppointDAO.Instance.Insert_Appoint_NoAcc(name, birth, addr, phone, id2, date))
                 {
-                    Error_Register = true;
-                    MessageBox.Show("Vui lòng nhập địa chỉ của bạn!", "Thông tin đặt hẹn không hợp lệ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ln_Success.Visible = true;
                 }
-            }
-            if (Error_Register == false)
-            {
-                if (tb_phone.Text.Length == 0)
-                {
-                    Error_Register = true;
-                    MessageBox.Show("Vui lòng nhập số điện thoại của bạn!", "Thông tin đặt hẹn không hợp lệ", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            if (string.IsNullOrEmpty(tb_dentist.Text))
-            {
-                Error_Register = true;
-                MessageBox.Show("Vui lòng chọn nha sĩ!", "Thông tin lịch hẹn không hợp lệ", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            if (Error_Register == false)
-            {
-                Client.Confirm_Appointment confirm_Appointment = new Client.Confirm_Appointment();
-                this.Hide();
-                confirm_Appointment.Show();
-                this.Show();
             }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void tb_date_ValueChanged(object sender, EventArgs e)
+        {
+            AppointDAO.Instance.Day = tb_date.Value.ToString("dd/MM/yyyy");
+            DateTime tmp1 = tb_date.Value;
+            DateTime tmp2 = tb_time.Value;
+            date_chosse = new DateTime(tmp1.Year, tmp1.Month, tmp1.Day, tmp2.Hour, tmp2.Minute, 0);
+
+            LoadListDentist(date_chosse);
+        }
+
+        private void tb_time_ValueChanged(object sender, EventArgs e)
+        {
+            AppointDAO.Instance.Hour = tb_date.Value.ToString("HH:mm");
+            DateTime tmp1 = tb_date.Value;
+            DateTime tmp2 = tb_time.Value;
+            date_chosse = new DateTime(tmp1.Year, tmp1.Month, tmp1.Day, tmp2.Hour, tmp2.Minute, 0);
+            LoadListDentist(date_chosse);
         }
     }
 }
