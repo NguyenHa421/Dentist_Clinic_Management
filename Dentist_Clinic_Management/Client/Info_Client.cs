a13986592100c0477ddf7a7888ab9866388099c4
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-01-15 11:28:52 +0700

    Update Info_Client.cs

--- Dentist_Clinic_Management/Client/Info_Client.cs
+++ Dentist_Clinic_Management/Client/Info_Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dentist_Clinic_Management.DAO_Dentist_Clinic_Management;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,10 @@ namespace Dentist_Clinic_Management.Client
         public Info_Client()
         {
             InitializeComponent();
+            tb_Name.Text = AccountDAO.Instance.GetClientName(ClientDAO.Instance.Phone, AccountDAO.Instance.Pass);
+            tb_Birth.Text = ClientDAO.Instance.GetClientBirth(ClientDAO.Instance.Phone, AccountDAO.Instance.Pass);
+            tb_Phone.Text = ClientDAO.Instance.GetClientPhone(ClientDAO.Instance.Phone, AccountDAO.Instance.Pass);
+            tb_Address.Text = ClientDAO.Instance.GetClientAddr(ClientDAO.Instance.Phone, AccountDAO.Instance.Pass);
         }
 
         private void bt_Ok_Click(object sender, EventArgs e)
